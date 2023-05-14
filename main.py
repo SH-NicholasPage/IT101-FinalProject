87c7211818e5ce6e540eaee63479025f90381672
SH-NicholasPage <npage@southhills.edu>
2023-05-14 14:34:16 -0400

    Added comment

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':     # Program start from here
     print ("Program is starting ... ")
     try:
         #CALL FUNCTIONS/METHODS FROM HERE
-        pass
+        pass #You can remove the pass operator when you add commands to the try
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
         GPIO.cleanup()  

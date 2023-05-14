90a4f67123963bd7ede4d3a551bec7ea31385eb9
SH-NicholasPage <npage@southhills.edu>
2023-05-14 14:33:07 -0400

    Updated main

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import RPi.GPIO as GPIO
 import MFRC522
 
 #YOUR CODE HERE
+
+if __name__ == '__main__':     # Program start from here
+    print ("Program is starting ... ")
+    try:
+        #CALL FUNCTIONS/METHODS FROM HERE
+        pass
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+        GPIO.cleanup()  
